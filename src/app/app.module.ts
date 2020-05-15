7a8f1647257c934ee4c52f6ba92cc46f54a28e74
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-05-15 14:20:25 +0300

    add highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
